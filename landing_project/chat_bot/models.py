bc307707a8d1dc7c7ae6e316510c2373dca6a796
AlexHackath0n <sasha.morozov023@yandex.ru>
2023-11-24 23:11:34 +0300

    edit model client

--- landing_project/chat_bot/models.py
+++ landing_project/chat_bot/models.py
@@ -3,14 +3,14 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 class Client(models.Model):
-    age = models.IntegerField(blank=True)
-    full_name = models.CharField(blank=True, max_length=128)
+    age = models.IntegerField(blank=True, null=True)
+    full_name = models.CharField(max_length=128, )
     email = models.EmailField()
     date_create = models.DateTimeField(auto_now_add=True)
-    current_profession = models.CharField(max_length=128, blank=True)
+    current_profession = models.CharField(max_length=128, blank=True, null=True)
     #Пока поставил int, надо будет обсудить тип данных для стажа работы
-    work_experience = models.IntegerField(blank=True)
-    salary = models.IntegerField(blank=True)
+    work_experience = models.IntegerField(blank=True, null=True)
+    salary = models.IntegerField(blank=True, null=True)
     #https://django-phonenumber-field.readthedocs.io/en/latest/index.html
     phone = PhoneNumberField(blank=False, region='RU')
 
