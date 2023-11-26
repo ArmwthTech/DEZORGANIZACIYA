8f2aa52b2c4227b570adc7981d3537f3e779fd23
AlexHackath0n <sasha.morozov023@yandex.ru>
2023-11-26 19:33:14 +0300

    edit model client, blank and null added

--- landing_project/chat_bot/models.py
+++ landing_project/chat_bot/models.py
@@ -5,14 +5,14 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Client(models.Model):
     age = models.IntegerField(blank=True, null=True)
     full_name = models.CharField(max_length=128, )
-    email = models.EmailField()
+    email = models.EmailField(blank=True, null=True,)
     date_create = models.DateTimeField(auto_now_add=True)
     current_profession = models.CharField(max_length=128, blank=True, null=True)
     #Пока поставил int, надо будет обсудить тип данных для стажа работы
     work_experience = models.IntegerField(blank=True, null=True)
     salary = models.IntegerField(blank=True, null=True)
     #https://django-phonenumber-field.readthedocs.io/en/latest/index.html
-    phone = PhoneNumberField(blank=False, region='RU')
+    phone = PhoneNumberField(blank=True, null=True, region='RU')
 
     def __str__(self):
         return f'Name {self.full_name}'
